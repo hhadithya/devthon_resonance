a84dc004a340c502effe0c4727bf812829c30dab
nethmalgunawardhana <nethmalgunawardhana@gmail.com>
2025-04-19 03:43:57 +0530

    Update email template parameter for collaborator invitation

--- src/components/research/CollaborativeOpportunities.tsx
+++ src/components/research/CollaborativeOpportunities.tsx
@@ -101,7 +101,7 @@ const CollaborativeOpportunities: React.FC<CollaborativeOpportunitiesProps> = ({
       
       // Prepare the template parameters
       const templateParams = {
-        to_name: researcher.firstName,
+        name: researcher.firstName,
         email: researcher.email,
         research_title: researchTitle,
         message: `You have been added as a collaborator to the research project "${researchTitle}". Please log in to view the details and contribute to the research.`,
